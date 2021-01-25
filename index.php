182cf6d83c1b205ba524baf40f58aa4c5eed1929
Toshko Todorov <toshko_montana@yahoo.com>
2021-01-25 11:28:41 +0200

    creating form

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Project 7</title>
+	</head>
+	<body>
+		<form method="POST" action="">
+			<p>input data</p>
+			<input type="text" name="input_data" placeholder="/project/subproject/method"/>
+			<input type="submit" name="submit" value="Send" />
+		</form>
+	</body>
+</html>
