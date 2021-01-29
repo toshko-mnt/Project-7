ef126bf30e3e38e1c8e5d633fbdfc2963b2ebca6
Toshko Todorov <toshko_montana@yahoo.com>
2021-01-29 18:14:01 +0200

    update index.php
    include add_output.php

--- index.php
+++ index.php
@@ -16,40 +16,8 @@ session_start();
 		<?php
 		include 'includes/chek_input.php';
 		include 'includes/create_db.php';
+		include 'includes/add_output.php';
 
-		if(isset($input_calls)){
-			$unique_key1 = [];
-			foreach($input_calls as $value){
-				foreach($unique_unit_1 as $value_unit_1){
-					if($value[0] == $value_unit_1){
-						$unique_key1[$value[0]][] = [$value[1],$value[2]];
-					}
-				}
-			}
-			$unique_key2 = [];
-			foreach($unique_key1 as $key1 => $value1){
-				foreach($value1 as $value2){
-					foreach($unique_unit_2 as $value_unit_2){
-						if($value2[0] == $value_unit_2){
-							$unique_key2[$key1][$value2[0]][] = $value2[1];
-						}
-					}
-				}
-			}
-			$unique_key3 = [];
-			foreach($unique_key2 as $key2 => $value3){
-				foreach($value3 as $key3 => $value4){
-					foreach($value4 as $value5){
-						$unique_key3[$key2][$key3][$value5] = $value5;
-					}	
-				}
-			}
-			$output_calls = [];
-			$output_calls = $unique_key3;
-			echo "<pre>";
-			print_r($output_calls);
-			echo "</pre>";
-		}
 		?>
 	</body>
 </html>
