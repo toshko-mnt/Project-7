02d640bc5350d6596002c56852d01981c6b5638d
Toshko Todorov <toshko_montana@yahoo.com>
2021-01-29 17:25:57 +0200

    update index.php
    include create_db.php

--- index.php
+++ index.php
@@ -15,24 +15,8 @@ session_start();
 		</form>
 		<?php
 		include 'includes/chek_input.php';
-		
-			if(isset($_SESSION['data'])){		
-				$input_calls = [];
-				$unit_1 = [];
-				$unit_2 = [];
-				$unique_unit_1 = [];
-				$unique_unit_2 = [];
-				for($i = 0; $i < count($_SESSION['data']); $i++){
-					$row_sess = $_SESSION['data'][$i];
-					$explode_row = explode('/' , $row_sess);
-					$input_calls[$i] = [$explode_row[1],$explode_row[2],$explode_row[3]];
-					$unit_1[$i] = $explode_row[1];
-					$unit_2[$i] = $explode_row[2];
-					$unique_unit_1 = array_unique($unit_1);
-					$unique_unit_2 = array_unique($unit_2);
-				}
-			}
-	
+		include 'includes/create_db.php';
+
 		if(isset($input_calls)){
 			$unique_key1 = [];
 			foreach($input_calls as $value){
