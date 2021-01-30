306fd6d7efe48630a50348f1d5ec87b0cfbb58d4
Toshko Todorov <toshko_montana@yahoo.com>
2021-01-30 11:02:14 +0200

    update form and html tag

--- index.php
+++ index.php
@@ -5,19 +5,26 @@ session_start();
 <html lang="en">
 	<head>
 		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<title>Project 7</title>
+		<link rel="stylesheet" href="css/styles.css">
 	</head>
 	<body>
-		<form method="POST" action="">
-			<p>input data</p>
-			<input type="text" name="input_data" placeholder="/project/subproject/method"/>
-			<input type="submit" name="submit" value="Send" />
-		</form>
-		<?php
-		include 'includes/chek_input.php';
-		include 'includes/create_db.php';
-		include 'includes/add_output.php';
-		include 'includes/print_output.php';
-		?>
+		<header>
+			<h1>Project 7 countAPI</h1>
+		</header>
+		<main>
+			<form method="POST" action="">
+				<label for="api">Enter API requests in the format: </br>/project/subproject/method</label>
+				<input type="text" name="input_data" id="api" size="30" autofocus />
+				<input type="submit" name="submit" value="Send" />
+			</form>
+			<?php
+			include 'includes/chek_input.php';
+			include 'includes/create_db.php';
+			include 'includes/add_output.php';
+			include 'includes/print_output.php';
+			?>
+		</main>
 	</body>
 </html>
